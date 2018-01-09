974c0d38f319a585f0d661c6cabb7a2c06e7feba
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2018-01-09 15:20:34 +0100

    zadanie-java-41 - ulepszone (50 liczb)

--- src/zbioryLosowe.java
+++ src/zbioryLosowe.java
@@ -10,10 +10,14 @@ public class zbioryLosowe {
 
         TreeSet<Integer> set = new TreeSet<>();
 
-        set.add(randomize.nextInt(1000));
-        set.add(randomize.nextInt(1000));
-        set.add(randomize.nextInt(1000));
-        set.add(randomize.nextInt(1000));
+        int licznik = 50;
+        double suma = 0;
+        int liczba = 0;
+        for (int i = 1; i <= licznik; i++) {
+            liczba = randomize.nextInt(1000);
+            set.add(liczba);
+            suma = suma + liczba;
+        }
 
         int size = set.size();
         System.out.println(set);
@@ -24,6 +28,13 @@ public class zbioryLosowe {
         System.out.print("Najwiekszy wygenerowany: ");
         System.out.println(set.last());
 
-        System.out.println("Srednia wynosi " + (randomize.nextInt(1000) + randomize.nextInt(1000) + randomize.nextInt(1000) + randomize.nextInt(1000))/4);
+        System.out.println("Srednia wynosi: " + suma / 50 );
+
+        System.out.println("Wszystkie liczby mniejsze od średniej to: ");
+        for (Integer integer : set) {
+            if (integer < (suma / 50)){
+                System.out.print(integer + " ");
+            }
+        }
     }
 }
