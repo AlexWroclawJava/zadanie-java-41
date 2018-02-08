288b4d40aab29172bc52aef320d751830fe948aa
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2018-02-08 19:25:02 +0100

    zadanie-java-41 - poprawione (dokładnie 50 liczb w zbiorze)

--- src/zbioryLosowe.java
+++ src/zbioryLosowe.java
@@ -10,31 +10,41 @@ public class zbioryLosowe {
 
         TreeSet<Integer> set = new TreeSet<>();
 
-        int licznik = 50;
         double suma = 0;
         int liczba = 0;
-        for (int i = 1; i <= licznik; i++) {
+        int rozmiarZbioru = 50;
+
+        while (set.size() < 50){
             liczba = randomize.nextInt(1000);
-            set.add(liczba);
-            suma = suma + liczba;
+            if (!set.contains(liczba)) {
+                set.add(liczba);
+                suma = suma + liczba;
+            }
+            else set.remove(liczba);
+            suma = suma;
         }
 
         int size = set.size();
         System.out.println(set);
 
-        System.out.print("Najmniejszy wygenerowany: ");
+
+        System.out.print("\nNajmniejsza wygenerowana liczba to: ");
         System.out.println(set.first());
 
-        System.out.print("Najwiekszy wygenerowany: ");
+        System.out.print("Największa wygenerowana liczba to: ");
         System.out.println(set.last());
 
-        System.out.println("Srednia wynosi: " + suma / 50 );
+        System.out.println("Średnia z 50 liczb wynosi: " + suma / 50 );
 
-        System.out.println("Wszystkie liczby mniejsze od średniej to: ");
+        System.out.println("Wszystkie liczby większe od średniej to: ");
         for (Integer integer : set) {
-            if (integer < (suma / 50)){
+            if (integer > (suma / 50)){
                 System.out.print(integer + " ");
             }
         }
+
+        if (set.size() == 50)
+        System.out.println("\n\n Rozmiar zbioru powinien wynosić dokładnie 50 i wynosi: " + set.size() + ", Test passed OK");
+        else System.out.println("\nObecny rozmiar zbioru wynosi: " + set.size() + ", Test failed");
     }
 }
